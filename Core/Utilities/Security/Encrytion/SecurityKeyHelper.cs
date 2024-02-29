ab2a199aa7a4537e27b25b739f31911e956ef0b0
Ramazan ESER <eserramazan43@gmail.com>
2024-02-29 21:48:25 +0300

    14. gün 2. bölümün ikinci commiti, bazı jwt helper vb. sınıflar yazıldı.

---
+++ Core/Utilities/Security/Encrytion/SecurityKeyHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Utilities.Security.Encrytion
+{
+    //Şifrelereme işlemleri olan her yerde bunları byt[] arrye şeklinde asp.net in anlayacağı şekilde vermemiz gerekiyor, basit bir string ile password oluşturamıyoruz.
+    public class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey) //Burada bir SecurityKey döndürmemiz gerekiyor. Yani TokenOptions dosyasında oluşturduğumuz "mysupersecretkeymysupersecretkey" keyini.
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));//Burada kullandığımız bazı şeyleri daha sonra mutlaka araştıralım.
+        }
+    }
+}
---
+++ Core/Utilities/Security/Encrytion/SigningCredentialsHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Encrytion
+{
+    public class SigningCredentialsHelper
+    {
+        public SigningCredentials CreateSigningCredentials(SecurityKey securityKey)//WebApi nin kullanabilece JWT lerin oluşturulabilmesi için Elimizdeki yani kullanıcı adı parola bir Credentials dır. Burada elimizde SecurityKey var, o sisteme kullanabilmemiz için bir tane anahtara ihtiyacımız vardı ya credential dediğimiz bizim anahtarımız oluyor, o yüzden burada parametre olarak elimizdeki securityke securitykey formatında vereceğiz ve o da bize o imzalama nesnesi döndürüyor olacak. 
+        {
+            return new SigningCredentials(securityKey,SecurityAlgorithms.HmacSha512Signature);//Asp net e sen şimdi bir hashing işlemi yapacaksın anahtar olarak parametrenin içinde verdiğim bu securityKey kullan, şifreleme olarak da güvenlik algoritmalarından securityAlgorithms.HmacSha512Signature yi kullan diyoruz. Aslında bu sınıf web apiye, asp nete hangi anahtarı ve hangi algoritmayı kullanacağı bilgisini veriyor.
+        }
+    }
+}
+
+//Capilot'un açıklaması.
+//Credential, Türkçe’de “delil”, “ehliyet”, “itimatname”, veya “yeterlilik belgesi” anlamlarına gelir 1. Genellikle yazılı formda sunulan bir kişinin kimliği, pozisyonu veya yetkisi hakkında bilgi içerir. Örneğin, basın kartı, ehliyet veya mesleki yeterlilik belgesi gibi deliller kişinin yetkisini veya niteliklerini gösterir. Kimlik doğrulama işlemlerinde de kullanılır.
